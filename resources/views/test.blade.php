ac02a1c84463caaa3e5b51b70e7c1abc333f4787
Mario Milunovic <mario.milunovic@gmail.com>
2022-09-13 14:49:57 +0200

    Preparation done

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Multishop') }}</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
