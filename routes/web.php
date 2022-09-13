6f0229a43965ac1840e3af3f321aef2a71d50079
Mario Milunovic <mario.milunovic@gmail.com>
2022-09-13 12:06:39 +0200

    Installed Tailwind

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+//TEST
+Route::get('/test',[TestController::class,'test'])->name('test');
 
 Route::get('/', function () {
     return view('welcome');
